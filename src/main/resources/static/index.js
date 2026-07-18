40802f99039ac9090346b63fec7d1fb813c8ee06
Srineha <psrineha.tech@gmail.com>
2026-07-18 12:56:16 +0530

    added random user feature

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -23,3 +23,22 @@ function toggleUser()
     var userGender=document.getElementById("user-gender");
     userGender.innerHTML=users[id].gender;
 }
+function randomUser()
+{
+    fetch('https://randomuser.me/api/').then(function(response){
+        return response.json();})
+        .then(function(data){
+            var userData=data.results[0];
+            var userImage=document.getElementById("user-image");
+            userImage.src=userData.picture.large;
+            var userName=document.getElementById("user-name");
+            userName.innerHTML=userData.name.first+" "+userData.name.last;
+            var userGender=document.getElementById("user-gender");
+            userGender.innerHTML=userData.gender;
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+        
+    
+}
