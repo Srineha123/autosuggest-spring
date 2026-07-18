0bdd0b5c4c410ac93c6c40c335266dc52989f941
Srineha <psrineha.tech@gmail.com>
2026-07-18 12:14:39 +0530

    added toggle user feature

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,25 @@
 // alert("Hi,Nice to Meet you  :)");
-console.log("Hello from js")
+console.log("Hello from JS!")
+var users=[
+    {
+        "name": "Virat Kohli",
+        "gender": "Male",
+        "img": "virat.avif"
+    },
+    {
+        "name":"Smriti Mandhana",
+        "gender": "Female",
+        "img": "smriti.jpeg"
+    }
+]
+var id=0;
+function toggleUser()
+{
+    id=(id+1)%2
+    var userImage=document.getElementById("user-image");
+    userImage.src=users[id].img;
+    var userName=document.getElementById("user-name");
+    userName.innerHTML=users[id].name;
+    var userGender=document.getElementById("user-gender");
+    userGender.innerHTML=users[id].gender;
+}
